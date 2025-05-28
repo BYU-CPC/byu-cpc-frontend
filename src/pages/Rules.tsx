c3b9577cd6e4ef6849a63a7183bf7d10134a795d
Josh Taylor <joshbtay@gmail.com>
2025-05-27 23:10:09 -0600

    more fix to rules

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Sidebar } from "../components/Sidebar";
-import { Link } from "@tanstack/react-router";
 export default function Rules() {
   return (
     <Sidebar title="Rules">
@@ -41,44 +40,6 @@ export default function Rules() {
               problems as many days as possible.
             </strong>
           </p>
-          <p className="large bold">Cheating</p>
-          <p>
-            This contest is meant as a tool for self-improvement. If you cheat,
-            you will be hurting your opportunities to improve as a programmer.
-            We recognize that cheating is possible and very tempting. Any form
-            of cheating will result in disqualification from the contest.
-            Cheating includes the following:
-          </p>
-          <ul className="list-disc pl-6">
-            <li>Submitting entire solutions that you did not write</li>
-            <li>
-              Submitting entire solutions that you do not mostly understand
-              &mdash; Using another person's implementation of a general
-              algorithm that you use to help solve a problem is okay
-            </li>
-            <li>Spoofing submissions</li>
-            <li>
-              Submitting code you had written previously on a separate account
-            </li>
-            <li>Using LLMs to solve problems for you</li>
-          </ul>
-          <p>Things that are not cheating include:</p>
-          <ul className="list-disc pl-6">
-            <li>
-              Using other peoples' implementations for general algorithms/data
-              structure &mdash; Using book code without completely understanding
-              it is okay
-            </li>
-            <li>
-              Discussing problems with others to understand their solutions
-            </li>
-            <li>
-              Using LLMs as an assistant to get you pointed in the right
-              direction
-            </li>
-            <li>Reading Codeforces tutorials</li>
-            <li>Finding hints online to solve problems</li>
-          </ul>
           <p className="small">
             If you notice any bugs or have any questions or concerns about the
             rules, website, or anything else, please contact Josh Taylor
